43af6bc1978ec40710816991d84b456d16bbfab6
Will Cronin <croninw9@students.rowan.edu>
2018-10-18 10:42:02 -0400

    Added 5529 v 2.2

--- Milestone_StrangerThings/Milestone1_5529/main.c
+++ Milestone_StrangerThings/Milestone1_5529/main.c
@@ -1,4 +1,4 @@
-#include <msp430.h> 
+#include <msp430.h>
 
 
 /**
@@ -7,16 +7,15 @@
 
 extern void LEDSetup();
 extern void timerSetup();
-extern void resetColor();
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	LEDSetup();
-	timerSetup();
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    LEDSetup();
+    timerSetup();
 
-	//UART CODE
-	P4SEL |= (BIT4+BIT5);
+    //UART CODE
+    P4SEL |= (BIT4+BIT5);
     UCA1CTL1 |= UCSWRST; //State machine reset + small clock initialization
     UCA1CTL1 |= UCSSEL_2;
     UCA1BR0 = 6;                //9600 baud *Determined by TI calculator(http://processors.wiki.ti.com/index.php/USCI_UART_Baud_Rate_Gen_Mode_Selection)
@@ -38,19 +37,12 @@ void timerSetup(void)
 {
     TA0CTL = TASSEL_2 + MC_1 + OUTMOD_7;        //SMCLK + upmode + reset/set
     TA0CCR0 = 0xFF;      //Full Cycle
-    TA0CCR1 = 255;        //Red LED
-    TA0CCR2 = 0;        //Green LED
-    TA0CCR3 = 0;        //Blue LED
+    //TA0CCR1 = 255;        //Red LED
+    //TA0CCR2 = 0;        //Green LED
+    //TA0CCR3 = 0;        //Blue LED
     TA0CCTL1 = OUTMOD_2;  //toggle
     TA0CCTL2 = OUTMOD_2;  //toggle
     TA0CCTL3 = OUTMOD_2;  //toggle
-}
-
-void resetColor()
-{
-    TA0CCR1 = 0x00; // Sets TA0CCR1
-    TA0CCR2 = 0x00; // Sets TA0CCR2
-    TA0CCR3 = 0x00; // Sets TA0CCR3
 }
 
 volatile int bitcounter = 0;
@@ -67,8 +59,8 @@ void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
 {
     switch(__even_in_range(UCA1IV,4))
       {
-      case 0:break;                             // Vector 0 - no interrupt
-      case 2: // Vector 2 - RXIFG
+      case USCI_NONE:break;                             // Vector 0 - no interrupt
+      case USCI_UCRXIFG: // Vector 2 - RXIFG
           switch(bitcounter)
               {
               case 0:
@@ -100,17 +92,17 @@ void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
                   UCA1TXBUF = UCA1RXBUF;      //Transmit bytes to next board
                   break;
               }
-              if(bitcounter != buffer - 1)
+              if(bitcounter != buffer)
               {
                   bitcounter += 1;
                }
-              else if (bitcounter == buffer - 1)
+              else if (bitcounter == buffer)
               {
                    //At the end of the message reset count in anticipation for the next message
                    bitcounter = 0;
               }
         break;
-        case 4:break;
+        case USCI_UCTXIFG:break;
         default: break;
       }
 }
@@ -128,22 +120,18 @@ __interrupt void USCI_A1_ISR(void)       //Interrupt occurs anytime an input/out
         UCA1TXBUF = UCA1RXBUF - 3;  //Takes 3 bytes off the code for LEDs and leaves the rest
         //__no_operation();           //Pauses the clock for a moment
         break;
-
     case 1:
         TA0CCR1 = (UCA1RXBUF);      //Sets RED section
         bitcounter += 1;
         break;
-
     case 2:
         TA0CCR2 = (UCA1RXBUF);      //Sets Green section
         bitcounter += 1;
         break;
-
     case 3:
         TA0CCR3 = (UCA1RXBUF);      //Sets Blue section
         bitcounter += 1;
         break;
-
     default:
         //while(!(UCA1IFG & UCTXIFG));
         //UCA1TXBUF = UCA1RXBUF - 3;  //Takes 3 bytes off the code for LEDs and leaves the rest
@@ -154,7 +142,6 @@ __interrupt void USCI_A1_ISR(void)       //Interrupt occurs anytime an input/out
         }
         break;
     }
-
 }*/
 /*
 {
@@ -167,22 +154,18 @@ __interrupt void USCI_A1_ISR(void)       //Interrupt occurs anytime an input/out
                buffer = UCA1RXBUF;
                bitcounter++;
                break;
-
            case 1:
                TA0CCR1 = (UCA1RXBUF);      //Sets RED section
                bitcounter++;
                break;
-
            case 2:
                TA0CCR2 = (UCA1RXBUF);      //Sets Green section
                bitcounter++;
                break;
-
            case 3:
                TA0CCR3 = (UCA1RXBUF);      //Sets Blue section
                bitcounter++;
                break;
-
            default:
                if(bitcounter <= buffer)
                {
